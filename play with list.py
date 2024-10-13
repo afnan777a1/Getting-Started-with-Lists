713d6b1181cd862f34f767184850efde80cea5bb
Afnan <alian777alian777a1@gmail.com>
2024-10-13 10:16:38 +0600

    h

---
+++ play with list.py
@@ -0,0 +1,13 @@
+L = [4, 5, 1, 2, 9, 7, 10, 8]
+print("Original list :", L)
+
+count = 0
+
+for i in L:
+    count = count + 1
+
+print("sum = ", count)
+
+avg = count/len(L)
+
+print("average = ", avg)
